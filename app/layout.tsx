42f23bd35b5dbf589ab25f4d6d0429f08d15164c
jin <flatupgym@gmail.com>
2025-10-04 17:19:33 +0900

    feat: Optimize GTM loading with next/script

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,16 +69,6 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        {/* <Partytown debug={false} forward={['dataLayer.push']} /> */}
-        {/* <script id="gtm-script" type="text/partytown" dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M5TLG6T7');
-          `,
-        }} /> */}
         <link rel="dns-prefetch" href="//static.readdy.ai" />
         <link rel="dns-prefetch" href="//lin.ee" />
         <link rel="dns-prefetch" href="//maps.google.com" />
@@ -177,6 +167,19 @@ export default function RootLayout({
       <body className={`${inter.className} ${pacifico.className}`}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5TLG6T7"
 height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M5TLG6T7');
+            `,
+          }}
+        />
         <Header />
         <main>
           {children}
