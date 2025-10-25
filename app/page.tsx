f625c6d696fbc94c5034af757be82495eb3d561a
jin <flatupgym@gmail.com>
2025-10-26 08:37:30 +0900

    fix: next/dynamic をクライアントコンポーネントに移動

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
+import ClientContentWrapper from '@/components/home/ClientContentWrapper';
 import HeroSection from '@/components/HeroSection';
-import dynamic from 'next/dynamic';
-
-// ClientContentを動的にインポートし、サーバーサイドレンダリングを無効にする
-const ClientContent = dynamic(() => import('@/components/home/ClientContent'), { ssr: false });
 
 export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen">
       <HeroSection />
-      {/* 動的にインポートされるセクションをClientContentコンポーネント内にラップします */}
-      <ClientContent />
+      <ClientContentWrapper />
     </main>
   );
 }
