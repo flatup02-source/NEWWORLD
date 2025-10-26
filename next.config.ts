8a407d0f9eb4b04240458c0153c6ced780d1d22d
jin <flatupgym@gmail.com>
2025-10-26 09:33:33 +0900

    fix: image remotePatterns fixed

--- next.config.ts
+++ next.config.ts
@@ -30,12 +30,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
   
